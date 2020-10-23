37da8f5fdc26081038461487704aef7b50e24053
s154472 <73245921+s154472@users.noreply.github.com>
2020-10-23 11:38:42 -0500

    Initial commit

---
+++ TestBalloon.java
@@ -0,0 +1,30 @@
+import java.awt.Color; 
+
+public class TestBalloon
+{
+	public static void main(String[] args)
+	{
+		// Create a Balloon called greenie, centered at x = 50, y = 100
+		// with radius 30 and color Color.GREEN: 
+
+		//	< statement not shown >		; 
+
+		// System.out.println("x = " + greenie.getX()); 
+		// System.out.println("y = " + greenie.getY()); 
+		// System.out.println("radius = " + greenie.getRadius()); 
+
+		// Call greenie's move method to move its center to (60, 110):
+
+		//	< statement not shown > 	; 
+
+		// System.out.println("x = " + greenie.getX()); 
+		// System.out.println("y = " + greenie.getY()); 
+		
+		// Call greenie's isOnBorder method to see if (81, 131)
+		// is on its border: 
+
+		// boolean onBorder = 	< expression not shown >	;
+
+		// System.out.println("(81, 131) is on the border: " + onBorder); 
+	}
+}
